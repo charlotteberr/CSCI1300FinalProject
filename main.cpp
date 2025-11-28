cd610996cfbcba75b25cfba06f20e328d84c9f40
charlottebe <cjbertanzetti@gmail.com>
2025-11-28 16:39:48 -0700

    added main

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include "Board.h"
+#include "Player.h"
+
+using namespace std;
+
+int main(){
+
+}
