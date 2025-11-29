e426fff7667df413a51b9e9f6f8200d8ef7bb77d
charlottebe <cjbertanzetti@gmail.com>
2025-11-28 17:39:17 -0700

    read in characters.txt, put info into array of Player type

--- main.cpp
+++ main.cpp
@@ -27,5 +27,32 @@ vector<string> split(string line, char delimiter){
 }
 
 int main(){
+    srand(time(0));
+    Board board;
+    Player characterInfo[5]; //for reading in characters.txt
+    Player players[2];
+
+    ifstream inFile;
+    inFile.open("characters.txt");
+    string line;
+    int i=0;
+    if(inFile.fail()){
+        cout<<"Error opening characters.txt"<<endl;
+        return 1;
+    }
+    else{
+        while(getline(inFile, line)){    // loads the 5 characters and their stats in characters.txt into an index of characterInfo
+            vector<string> parts=split(line,'|');
+            string n=parts[0];
+            int exp=stoi(parts[1]);
+            int acc=stoi(parts[2]);
+            int eff=stoi(parts[3]);
+            int ins=stoi(parts[4]);
+            int dp=stoi(parts[5]);
+
+            characterInfo[i]=Player(n, exp, acc, eff, ins, dp);
+            i++;
+        }
+    }
 
 }
